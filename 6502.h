20b412022184ccf0723f08e04bc199406d2e86a9
xxm0703 <xxm0703@abv.bg>
2018-08-05 21:27:14 +0300

    Formatting code

--- 6502.h
+++ 6502.h
@@ -12,15 +12,15 @@
 #define zero(x) (x ? cpu->flags &= ~Z : cpu->flags |= Z)
 #define nega(x) (x >> 7 ? cpu->flags |= N : cpu->flags &= ~N)
 
-typedef struct{
-  int8_t flags, acc, X, Y;
-  uint8_t SP;
-  uint16_t PC;
+typedef struct {
+        int8_t flags, acc, X, Y;
+        uint8_t SP;
+        uint16_t PC;
 } cpu_t;
 
-typedef struct{
-  cpu_t *cpu;
-  int8_t *mem, *ins;
+typedef struct {
+        cpu_t *cpu;
+        int8_t *mem, *ins;
 } machine_t;
 
 inline uint16_t ind(uint8_t, int8_t);
